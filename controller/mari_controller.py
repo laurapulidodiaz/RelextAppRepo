024c11ad9857dad6e5a3b5cf891c15f30967d896
camelom <89279985+camelom@users.noreply.github.com>
2021-08-22 11:14:16 -0400

    Cambio controladores mari
    .

---
+++ controller/mari_controller.py
@@ -0,0 +1,42 @@
+from controller import local_to_dataframes as ld
+import plotly.express as px
+
+def lineplot(tipo,pais,dpto,pos,cat,mesini,anoini,mesfin,anofin):
+    df_exports, df_imports, df_exports_cundinamarca, df_imports_cundinamarca = ld.cargar_dataframes(anoini,mesini,anofin,mesfin)
+
+    if tipo=="Exportaciones":
+        df=df_exports
+        if dpto != "":
+            df=df[df["Departamento de procedencia"] == dpto]
+        if pais != "":
+            df=df[df["Código país destino"]==pais]
+        if pos != "":
+            df=df.groupby(by=["Fecha","Descripción Arancelaria"]).sum()[["Total valor FOB doláres de la posición"]].reset_index()
+            fig=px.line(df[df["Descripción Arancelaria"]==pos], x="Fecha", y="Total valor FOB doláres de la posición", title=f'Valor FOB (USD) para la posición arancelaria {pos}, país:{pais}, departamento:{dpto}', labels={"FOBDOL": "Valor FOB (USD)"})
+        else:
+            if pos == "" and  cat!= "":
+                df=df.groupby(by=["Fecha","Descriptiva - SCN - NUEVA BASE 2015"]).sum()[["Total valor FOB doláres de la posición"]].reset_index()
+                fig=px.line(df_plot[df_plot["Descriptiva - SCN - NUEVA BASE 2015"]==cat], x="Fecha", y="Total valor FOB doláres de la posición", title=f'Valor FOB (USD) para la categoría arancelaria {cat}, país:{pais}, departamento:{dpto}', labels={"FOBDOL": "Valor FOB (USD)"})
+            else:
+                print(df_exports.columns)
+                df=df.groupby(by=["Fecha"]).sum()[["Total valor FOB doláres de la posición"]].reset_index()
+                fig=px.line(df, x="Fecha", y="Total valor FOB doláres de la posición", title=f'Valor FOB (USD), país:{pais}, departamento:{dpto}', labels={"FOBDOL": "Valor FOB (USD)"})
+
+    elif tipo == "Importaciones":
+        df=df_imports
+        if dpto != "":
+            df=df[df["Departamento del importador"] == dpto]
+        if pais != "":
+            df=df[df["País de procedencia"]==pais]
+        if pos != "":
+            df=df.groupby(by=["Fecha","Descripción Arancelaria"]).sum()[["Valor CIF dólares de la mercancía"]].reset_index()
+            fig=px.line(df[df["Descripción Arancelaria"]==pos], x="Fecha", y="Valor CIF dólares de la mercancía", title=f'Valor CIF (USD) para la posición arancelaria {pos}, país:{pais}, departamento:{dpto}', labels={"Valor CIF dólares de la mercancía": "Valor CIF (USD)"})
+        else:
+            if pos == "" and  cat!= "":
+                df=df.groupby(by=["Fecha","Descriptiva - SCN - NUEVA BASE 2015"]).sum()[["Valor CIF dólares de la mercancía"]].reset_index()
+                fig=px.line(df[df["Descriptiva - SCN - NUEVA BASE 2015"]==cat], x="Fecha", y="Valor CIF dólares de la mercancía", title=f'Valor CIF (USD) para la categoría arancelaria {cat}, país:{pais}, departamento:{dpto}', labels={"Valor CIF dólares de la mercancía": "Valor CIF (USD)"})
+
+            else:
+                df=df.groupby(by=["Fecha"]).sum()[["Valor CIF dólares de la mercancía"]].reset_index()
+                fig=px.line(df, x="Fecha", y="Valor CIF dólares de la mercancía", title=f'Valor CIF (USD), país:{pais}, departamento:{dpto}', labels={"Valor CIF dólares de la mercancía": "Valor CIF (USD)"})
+    return fig
---
+++ view/mari_view.py
@@ -0,0 +1,70 @@
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+import dash_core_components as dcc
+from controller import consulta_controller as controller
+import plotly.express as px
+from controller import mari_controller as mari
+from controller import local_to_dataframes as ld
+
+df_exports, df_imports, df_exports_cundinamarca, df_imports_cundinamarca = ld.cargar_dataframes(2020,"Enero",2020,"Diciembre")
+MONTHS = ["Enero","Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio","Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+years=[2021,2020,2019,2018,2017]
+
+layout = html.Div([
+    dbc.Alert([
+        html.H4("Iniciar", className="alert-heading", style={"font-size":"20px","padding-top":"8px", "font-weight":"400"}),
+        html.P(
+            "Por favor, haz tu selección en algunos filtros para obtener información de registros históricos. "
+            " Si es la primera vez que ingresas al sistema quizás quieras empezar por uno de los productos populares en exportaciones en Cundinamarca.",
+            style = {"margin-bottom":"0.2rem"}
+             ),],
+            color="info"),
+    html.Label(["Tipo",dcc.Dropdown(id='Tipo-dropdown', clearable=False, value="Exportaciones",
+            options=[{'label': "Exportaciones", 'value': "Exportaciones"},
+                {'label': "Importaciones", 'value': "Importaciones"}]),
+    html.Label(["País destino o procedencia", dcc.Dropdown(id="Pais-dropdown",clearable=True,
+                searchable=True, value="",options=[{'label': c, 'value': c}
+                for c in df_exports['Código país destino'].drop_duplicates().sort_values().values]), #Cambiar por diccionario actualizado
+    html.Label(["Departamento destino o procedencia", dcc.Dropdown(id="Dpto-dropdown",clearable=True,
+                searchable=True, value="",options=[{'label': c, 'value': c}
+                for c in df_exports['Departamento de procedencia'].drop_duplicates().values]),#Cambiar a dict
+    html.Label(["Posición arancelaria", dcc.Dropdown(id="Pos-dropdown",clearable=True,
+                searchable=True, value="",options=[{'label': c, 'value': c}
+                for c in df_exports['Descripción Arancelaria'].drop_duplicates().sort_values().values]),
+    html.Label(["SCN - BASE 2015", dcc.Dropdown(id="Cat-dropdown",clearable=True,
+                searchable=True, value="",options=[{'label': c, 'value': c}
+                for c in df_exports['Descriptiva - SCN - NUEVA BASE 2015'].drop_duplicates().sort_values().values]),
+    html.Label(["Mes inicial", dcc.Dropdown(id="Mesini-dropdown",clearable=True,
+                searchable=True, value="",options=[{'label': c, 'value': c} for c in MONTHS]),
+    html.Label(["Año inicial", dcc.Dropdown(id="Anoini-dropdown",clearable=True,
+                searchable=True, value="",options=[{'label': c, 'value': c} for c in years]),
+    html.Label(["Mes final", dcc.Dropdown(id="Mesfin-dropdown",clearable=True,
+                searchable=True, value="",options=[{'label': c, 'value': c} for c in MONTHS]),
+    html.Label(["Año final", dcc.Dropdown(id="Anofin-dropdown",clearable=True,
+                searchable=True, value="",options=[{'label': c, 'value': c} for c in years]),
+    ])
+    ])
+    ])
+    ])
+    ])
+    ])
+    ])
+    ])
+    ]),
+])
+# Define callback to update graph
+#callback(
+    #Output('graph', 'figure'),
+    #Input("Tipo-dropdown", "value"),
+    #Input("Pais-dropdown","value"),
+    #Input("Dpto-dropdown","value"),
+    #Input("Pos-dropdown","value"),
+    #Input("Cat-dropdown","value"),
+    #Input('Mesini-dropdown', 'value'),
+    #Input('Anoini-dropdown', 'value'),
+    #Input('Mesfin-dropdown', 'value'),
+    #Input('Anofin-dropdown', 'value')
+#)
+
+mari.lineplot("Exportaciones","","","","","Enero",2020,"Diciembre",2020)
+#mari.lineplot(tipo,pais,dpto,pos,cat,"Enero",2020,"Diciembre",2020)
