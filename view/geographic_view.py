e694fbf513f9a14dc5cd4414d07290959efcbe97
Juliana Dueñas V <50778805+JulianaDV@users.noreply.github.com>
2021-08-31 23:09:44 -0500

    agregué el titulo de la gráfica en el mapa

--- view/geographic_view.py
+++ view/geographic_view.py
@@ -15,6 +15,9 @@ def cargar_geo():
                 style = {"margin-bottom":"0.2rem"}
                  ),],
                 color="info"),
+        html.P("Exportaciones según el país de destino", style={"font-weight": "600", "font-size": "16px", "margin-top": "24px"}),
+        html.P("Según el valor total de las exportaciones en USD.",
+               style={"font-weight": "normal", "font-size": "13px", "margin-top": "-16px"}),
         dcc.Graph(figure = grafico, style={"font-weight":"normal","font-size":"13px","margin-left":"24px","margin-top":"-18px"})
     ])
 
@@ -25,6 +28,10 @@ def cargar_geo_filtros(tipo=1,pos=None,cat=None,anoini=2021,mesini='Mayo',anofin
     grafico= ugc.update_choropleth_world(tipo,anoini,mesini,anofin, mesfin)
 
     layout = html.Div([
+        html.P("Exportaciones según el país de destino",
+               style={"font-weight": "600", "font-size": "16px", "margin-top": "24px"}),
+        html.P("Según el valor total de las exportaciones en USD.",
+               style={"font-weight": "normal", "font-size": "13px", "margin-top": "-16px"}),
         dcc.Graph(figure = grafico, style={"font-weight":"normal","font-size":"13px","margin-left":"24px","margin-top":"-18px"})
     ])
 
