4858c37688fee652100df84025dc049d948e660d
camelom <89279985+camelom@users.noreply.github.com>
2021-09-02 00:36:16 -0400

    .
    .

--- prueba_func.py
+++ prueba_func.py
@@ -4,13 +4,24 @@ from controller import mari_controller as mc
 import pandas as pd
 
 
+def dict_paises():
+    paises = pd.read_csv("data/CSV/paisesall.csv", low_memory=False, delimiter=";").set_index("value")
+    paises_dict = paises.to_dict()
+    paises_dict = paises_dict["code"]
+
+    return paises_dict
+
+
+paises_dict=dict_paises()
+
 df,df_exports=mc.lineplot(2,"Enero",2020,"Diciembre",2020,"","","")
 
 
+df_exports["País destino"]=df_exports["País destino"].replace(paises_dict)
 df_exports["País destino"]=df_exports["País destino"].astype(int)
-
 #df_imports["País origen"]=df_imports["País origen"].astype(int)
 print("PRUEBAAAAAAAAAAAAAAAA")
-print(df_imports.info())
-print(df_imports.head(10))
+print(df_exports.info())
+print(df_exports.head(10))
+
 
