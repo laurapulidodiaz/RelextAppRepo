d5265609a5a8bef90ccaecc2a1af34a8e54d9975
palleja03 <70347550+palleja03@users.noreply.github.com>
2021-09-03 16:28:55 -0500

    Table with export button

--- controller/table_controller.py
+++ controller/table_controller.py
@@ -37,20 +37,21 @@ def get_table(tipo=1, anoini=2021, mesini='Mayo', anofin=2021, mesfin='Mayo'):
         ],
         data=df.to_dict('records'),  # the contents of the table
         hidden_columns=[col for col in df.columns if col not in init_columns],
+        export_format='xlsx',
         editable=False,              # allow editing of data inside all cells
         filter_action="native",     # allow filtering of data by user ('native') or not ('none')
         sort_action="native",       # enables data to be sorted per-column by user or not ('none')
-        sort_mode="multi",         # sort across 'multi' or 'single' columns
-        column_selectable="multi",  # allow users to select 'multi' or 'single' columns
-        row_selectable="multi",     # allow users to select 'multi' or 'single' rows
+        sort_mode="single",         # sort across 'multi' or 'single' columns
+        # column_selectable="multi",  # allow users to select 'multi' or 'single' columns
+        # row_selectable="multi",     # allow users to select 'multi' or 'single' rows
         row_deletable=False,       # choose if user can delete a row (True) or not (False)
-        selected_columns=[],        # ids of columns that user selects
-        selected_rows=[],           # indices of rows that user selects
+        # selected_columns=[],        # ids of columns that user selects
+        # selected_rows=[],           # indices of rows that user selects
         page_action="native",       # all data is passed to the table up-front or not ('none')
         page_current=0,             # page number that user is on
         page_size=15,                # number of rows visible per page
         style_cell={                # ensure adequate header width when text is shorter than cell's text
-            'minWidth': 50, 'maxWidth': 95, 'width': 50
+            'minWidth': 30, 'maxWidth': 180, 'width': 30
         },
         #style_cell_conditional=[    # align text columns to left. By default they are aligned to right
         #     {
@@ -58,8 +59,8 @@ def get_table(tipo=1, anoini=2021, mesini='Mayo', anofin=2021, mesfin='Mayo'):
         #         'textAlign': 'left'
         #     } for c in ['country', 'iso_alpha3']
         # ],
-        # style_data={                # overflow cells' content into multiple lines
-        #     'whiteSpace': 'normal',
-        #     'height': 'auto'
-        # }
+        style_data={                # overflow cells' content into multiple lines
+            'whiteSpace': 'normal',
+            'height': 'auto'
+        }
     )
